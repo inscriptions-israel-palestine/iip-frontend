4e33dfb05b461b296fa27272e706025132d92749
cmroughan <cmroughan@nyu.edu>
2023-11-21 23:45:52 -0500

    adding subpages for each language

---
+++ src/routes/wordlist/hebrew/+page.server.ts
@@ -0,0 +1,8 @@
+import { env } from '$env/dynamic/public'
+
+export async function load() {
+    const response = await fetch(`${env.PUBLIC_API_URL}/wordlist/hebrew`);
+    const wordlist = await response.json();
+
+    return { words: wordlist };
+}
