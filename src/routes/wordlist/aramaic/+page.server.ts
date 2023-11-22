0978b7073f5e97ecac67c1eda89242bec375eac3
cmroughan <cmroughan@nyu.edu>
2023-11-22 00:15:50 -0500

    adjusted subpages to use dynamic route parameters

--- src/routes/wordlist/aramaic/+page.server.ts
+++
@@ -1,8 +0,0 @@
-import { env } from '$env/dynamic/public'
-
-export async function load() {
-    const response = await fetch(`${env.PUBLIC_API_URL}/wordlist/aramaic`);
-    const wordlist = await response.json();
-
-    return { words: wordlist };
-}
