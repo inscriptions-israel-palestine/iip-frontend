a7496b614e83d0922946f92a5827cd3b18d0d25f
Charles Pletcher <pletcher@users.noreply.github.com>
2023-12-03 13:39:54 +0100

    Fix Zotero API calls, add biblScope data to Zotero response

--- src/lib/components/ZoteroItem.svelte
+++ src/lib/components/ZoteroItem.svelte
@@ -1,12 +1,19 @@
 <script lang="ts">
-	import type { ZoteroItem } from "$lib/types/zotero.type";
+	import type { BibliographicEntry } from '$lib/types/inscription.type';
+	import type { ZoteroItem } from '$lib/types/zotero.type';
 
-    export let item: ZoteroItem;
+	export let item: ZoteroItem & BibliographicEntry;
+
+	$: console.log(item);
 </script>
 
 <div class="flex-auto text-sm prose prose-stone prose-p">
-    <div class="flex prose prose-stone">
-        {@html item.bib}
-        <a class="text-xs" href={item.links.alternate.href}>Zotero</a>
-    </div>
+	<div class="flex prose prose-stone">
+		{@html item.bib}
+		<a class="text-xs" href={item.links.alternate.href}>Zotero</a>
+	</div>
+	<div>
+		{item.bibl_scope_unit}
+		{item.bibl_scope}
+	</div>
 </div>
